acbbdd49ec04d8c12476320252a9c2cb74003f0c
keer2345 <keer2345@gmail.com>
2019-10-12 22:03:19 +0800

    django-2-by-example/ch02.md taggit

--- django-2-by-example/mysite/blog/models.py
+++ django-2-by-example/mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -31,6 +32,8 @@ class Post(models.Model):
     objects = models.Manager()  # The default manager
     published = PublishedManager()  # Our custom manager
 
+    tags = TaggableManager()
+
     class Meta:
         ordering = ('-publish', )
 
@@ -43,3 +46,21 @@ class Post(models.Model):
                            self.publish.year, self.publish.month,
                            self.publish.day, self.slug
                        ])
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created', )
+
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name, self.post)
